5743893b7db34019b1ce74bb9f30e1c1f09d687a
onemonster <onemonsterceo@gmail.com>
2017-02-01 12:08:26 +0900

    Cleaned up code to follow standard js. Cleaned up mongoose code so it doesn't open and close so often.

--- information.js
+++ information.js
@@ -1,101 +1,83 @@
-var xml2js = require('xml2js');
-var parser = new xml2js.Parser();
-var fs = require('fs');
-var moment = require('moment');
+var xml2js = require('xml2js')
+var parser = new xml2js.Parser()
+var fs = require('fs')
+var moment = require('moment')
 
-var mongoose = require('mongoose');
-var Bluebird = require('bluebird');
+var mongoose = require('mongoose')
+var Bluebird = require('bluebird')
 
-var Restaurant = require('./models/restaurant.js').model;
+var Restaurant = require('./models/restaurant.js').model
 
-function updateInformation(callback) {
-    mongoose.connect('mongodb://localhost/restaurant');
-    var db = mongoose.connection;
+function updateInformation (callback) {
+  Restaurant.find(function (find_error, restaurants) {
+    var removeTasks = []
+    for (var i = 0; i < restaurants.length; i++) {
+      removeTasks.push(restaurants[i].remove())
+    }
 
-    db.on('error', console.error.bind(console, 'connection error:'));
-    db.once('open', function () {
-        Restaurant.find(function (find_error, restaurants) {
-            var removeTasks = [];
-            for (var i = 0; i < restaurants.length; i++)
-                removeTasks.push(restaurants[i].remove());
-
-            Bluebird.all(removeTasks).then(function () {
-                var data = fs.readFileSync(__dirname + '/public/information.xml', 'utf8');
-                parser.parseString(data, function (parse_error, body) {
-                    if (!parse_error) {
-                        var information = body["information"];
-                        var names = information["name"][0]["item"];
-                        var operatingHours = information["operating_hour"][0]["item"];
-                        var locations = information["location"][0]["item"];
-
-                        var saveTasks = [];
-                        for (var i = 0; i < names.length; i++) {
-                            var restaurant = new Restaurant({
-                                name: names[i],
-                                operatingHour: operatingHours[i],
-                                location: locations[i]
-                            });
-                            saveTasks.push(restaurant.save());
-                        }
-
-                        Bluebird.all(saveTasks).then(function (elements) {
-                            var result = {time: moment(new Date()).format("YYYY-MM-DD HH:mm"), data: elements};
-                            fs.writeFileSync(__dirname + "/public/jsons/information.json", JSON.stringify(result));
-                            mongoose.disconnect();
-                            callback(true);
-                        }, function (db_error) {
-                            mongoose.disconnect();
-                            callback(false);
-                        });
-                    }
-                });
-            }, function (db_error) {
-                mongoose.disconnect();
-                callback(false);
-            });
-        });
-    });
+    Bluebird.all(removeTasks).then(function () {
+      var data = fs.readFileSync(__dirname + '/public/information.xml', 'utf8')
+      parser.parseString(data, function (parse_error, body) {
+        if (!parse_error) {
+          var information = body['information']
+          var names = information['name'][0]['item']
+          var operatingHours = information['operating_hour'][0]['item']
+          var locations = information['location'][0]['item']
+          var saveTasks = []
+          for (var i = 0; i < names.length; i++) {
+            var restaurant = new Restaurant({
+              name: names[i],
+              operatingHour: operatingHours[i],
+              location: locations[i]
+            })
+            saveTasks.push(restaurant.save())
+          }
+          Bluebird.all(saveTasks).then(function (elements) {
+            var result = { time: moment(new Date()).format('YYYY-MM-DD HH:mm'), data: elements }
+            fs.writeFileSync(__dirname + '/public/jsons/information.json', JSON.stringify(result))
+            callback(true)
+          }, function (db_error) {
+            callback(false)
+          })
+        }
+      })
+    }, function (db_error) {
+      callback(false)
+    })
+  })
 }
 
-function viewInformation(callback) {
-    fs.readFile(__dirname + "/public/jsons/information.json", {encoding: "utf8"}, function (error, data) {
-        if (!error) {
-            callback(JSON.parse(data));
-        }
-        else {
-            mongoose.connect('mongodb://localhost/restaurant');
-            var db = mongoose.connection;
-
-            db.on('error', console.error.bind(console, 'connection error:'));
-            db.once('open', function () {
-                Restaurant.find(function (db_error, restaurants) {
-                    var result = {time: moment(new Date()).format("YYYY-MM-DD HH:mm"), data: restaurants};
-                    fs.writeFileSync(__dirname + "/public/jsons/information.json", JSON.stringify(result));
-                    mongoose.disconnect();
-                    callback(result);
-                });
-            });
-        }
-    });
+function viewInformation (callback) {
+  fs.readFile(__dirname + '/public/jsons/information.json', {encoding: 'utf8'}, function (error, data) {
+    if (!error) {
+      callback(JSON.parse(data))
+    } else {
+      Restaurant.find(function (db_error, restaurants) {
+        var result = {time: moment(new Date()).format('YYYY-MM-DD HH:mm'), data: restaurants}
+        fs.writeFileSync(__dirname + "/public/jsons/information.json", JSON.stringify(result))
+        callback(result)
+      })
+    }
+  })
 }
 
-function getLatestTime() {
-    var data = fs.readFileSync(__dirname + "/public/jsons/information.json", "utf8");
-    return {latest: JSON.parse(data).time};
+function getLatestTime () {
+  var data = fs.readFileSync(__dirname + '/public/jsons/information.json', 'utf8')
+  return { latest: JSON.parse(data).time }
 }
 
 module.exports = {
-    view: function (callback) {
-        viewInformation(function (result) {
-            callback(result);
-        });
-    },
-    update: function (callback) {
-        updateInformation(function (success) {
-            callback(success);
-        });
-    },
-    latest: function (callback) {
-        callback(getLatestTime());
-    }
-};
+  view: function (callback) {
+    viewInformation(function (result) {
+      callback(result)
+    })
+  },
+  update: function (callback) {
+    updateInformation(function (success) {
+      callback(success)
+    })
+  },
+  latest: function (callback) {
+    callback(getLatestTime())
+  }
+}
--- routes/rate.js
+++ routes/rate.js
@@ -1,176 +1,116 @@
 /**
  * Wednesday, August 24, 2016
  * Ilhyun Jo
- * 
+ *
  * define http verbs for rating a meal and retrieving rating info
- * 
- * TODO: Make some better names for routes 
  **/
 
-var express = require('express');
-var router = express.Router();
-var mongoose = require('mongoose');
-var Meal = require('../models/meal.js').model;
-var menu = require('../menu.js');
+var express = require('express')
+var router = express.Router()
+var Meal = require('../models/meal')
+var menu = require('../menu')
 
-router.get('/all', function (req, res) {
-	var db = mongoose.connect('mongodb://localhost/meal').connection;
-	db.on('error', console.error.bind(console, 'connection error:'));
-	db.once('open', function() {
-		Meal.find(function(error, mealList) {
-			if(error) console.error.bind(console, 'connection error:');
-			else res.send(mealList);
-			mongoose.disconnect();
-		});;
-	});
-});
+router.get('/all', function (req, res, next) {
+  Meal.find(function (error, mealList) {
+    if (error) {
+      return next(error)
+    } else {
+      return res.send(mealList)
+    }
+  })
+})
 
-router.get('/view', function (req, res) {
-	db = mongoose.connect('mongodb://localhost/meal').connection;
-	db.on('error', console.error.bind(console, 'connection error:'));
-	db.once('open', function() {
-		menu.crawl(req.query.date, function(resultString) {
-			var result = JSON.parse(resultString);
-			var menuDict = {};
-			var menuNames = [];
-			var ratedMenu = [];
-			for(var i in result.data) {
-				var restaurant = result.data[i];
-				for(var j in restaurant.foods) {
-					var meal = restaurant.foods[j];
-					menuNames.push(meal.name);
-					if(meal.name in menuDict) {
-						menuDict[meal.name].push(restaurant.restaurant);
-					} else {
-						menuDict[meal.name] = [restaurant.restaurant];
-					}
-				}
-			}
-			Meal.find({ name: {$in: menuNames} }, function (error,doc) {
-				if(error) console.error.bind(console, 'Meal.findOne error:');
-				else { 
-					for(var k in doc) {
-						if(menuDict[doc[k].name].contains(doc[k].restaurant)) {
-							ratedMenu.push(doc[k]);
-						}
-					}
-					for(var l in ratedMenu) {
-						var ratedMeal = ratedMenu[l];
-						if(result.data.indexOfRestaurant(ratedMeal.restaurant) > -1) {
-							var foodArray = result.data[result.data.indexOfRestaurant(ratedMeal.restaurant)].foods;
-							var unratedMealList = foodArray.sublistOfFoodNamed(ratedMeal.name);
-							for(var m in unratedMealList) {
-								var unratedMeal = unratedMealList[m];
-								unratedMeal["rating"] = ratedMeal.rating;
-								unratedMeal["numberOfRatings"] = ratedMeal.numberOfRatings;
-							}
-						}
-					}
-					res.send(result);
-					mongoose.disconnect();
-				}
-			});
-		});
-	});
-});
+router.get('/view', function (req, res, next) {
+  if (!req.query.date) {
+    var err = new Error('Date missing from query')
+    err.status = 401
+    return next(err)
+  }
+  menu.crawl(req.query.date, function (resultString) {
+    var result = JSON.parse(resultString)
+    var menus = []
+    for (var i in result.data) {
+      var restaurant = result.data[i]
+      for (var j in restaurant.foods) {
+        var meal = restaurant.foods[j]
+        menus.push({
+          meal: meal,
+          restaurant: restaurant.restaurant
+        })
+      }
+    }
+    for (var k in menus) {
+      Meal.findOne({ name: menus[k].meal.name, restaurant: menus[k].restaurant }, function (error, meal) {
+        if (error) {
+          return next(error)
+        } else if (meal) {
+          menus[k].meal.rating = meal.rating
+          menus[k].meal.numberOfRatings = meal.numberOfRatings
+        }
+      })
+    }
+    return res.send(result)
+  })
+})
 
-Array.prototype.contains = function(obj) {
-	var i = this.length;
-	while (i--) {
-		if (this[i] == obj) {
-			return true;
-		}
-	}
-	return false;
-}
+router.get('/restaurant/:restaurant', function (req, res, next) {
+  Meal.find({ restaurant: req.params.restaurant }, function (error, meals) {
+    if (error) {
+      return next(error)
+    } else {
+      res.send(meals)
+    }
+  })
+})
 
-Array.prototype.indexOfRestaurant = function(restaurantName) {
-	for (var i in this) {
-		if (this[i].restaurant === restaurantName) {
-			return i;
-		}
-	}
-	return -1;
-}
+router.get('/restaurant/:restaurant/meal/:meal', function (req, res, next) {
+  Meal.findOne({ name: req.params.meal, restaurant: req.params.restaurant }, function (error, meal) {
+    if (error) {
+      return next(error)
+    } else {
+      return res.send(meal)
+    }
+  })
+})
 
-Array.prototype.sublistOfFoodNamed = function(foodName) {
-	var foodList = [];
-	for (var i in this) {
-		if (this[i].name === foodName) {
-			foodList.push(this[i]);
-		}
-	}
-	return foodList;
-}
+router.post('/', function (req, res, next) {
+  if (!req.body.key || req.body.key !== 'siksha1996') {
+    var err = new Error('Need correct api key')
+    err.status = 401
+    return next(err)
+  }
+  if (req.body.meal && req.body.restaurant && req.body.rating) {
+    Meal.findOne({ name: req.body.meal, restaurant: req.body.restaurant }, function (error, meal) {
+      if (error) {
+        return next(error)
+      }
+      if (!meal) {
+        new Meal({
+          name: req.body.meal,
+          restaurant: req.body.restaurant,
+          rating: req.body.rating,
+          numberOfRatings: '1'
+        }).save(function (error, meal) {
+          if (error) {
+            return next(error)
+          }
+        })
+      } else {
+        meal.rating = (Number(meal.numberOfRatings) * Number(meal.rating) + Number(req.body.rating)) / (Number(meal.numberOfRatings) + 1)
+        meal.numberOfRatings += 1
+        meal.save(function (error, meal) {
+          if (error) {
+            return next(error)
+          }
+        })
+      }
+      return res.end('Rating successful')
+    })
+  } else {
+    var err = new Error('Missing data from form')
+    err.status = 401
+    return next(err)
+  }
+})
 
-router.get('/:restaurant', function (req, res) {
-	mongoose.connect('mongodb://localhost/meal');
-	var db = mongoose.connection;
-	db.on('error', console.error.bind(console, 'connection error:'));
-	db.once('open', function() {
-		Meal.find({ restaurant: req.params.restaurant }, function(error, mealList) {
-			if(error) console.error.bind(console, 'Meal.find error:');
-			else res.send(mealList);
-			mongoose.disconnect();
-		});
-	});
-});
-
-router.get('/:restaurant/:meal', function (req, res) {
-	mongoose.connect('mongodb://localhost/meal');
-	var db = mongoose.connection;
-	db.on('error', console.error.bind(console, 'connection error:'));
-	db.on('open', function () {
-		var rating;
-		var numberOfRatings;
-		Meal.findOne({ name: req.params.meal, restaurant: req.params.restaurant }, function (error, meal) {
-			if(error) console.error.bind(console, 'Meal.findOne error:');
-			else if(!meal) { rating = 0; numberOfRatings = 0 }
-			else { rating = meal.rating; numberOfRatings = meal.numberOfRatings }
-			res.send({
-				meal: req.params.meal,
-				restaurant: req.params.restaurant,
-				rating: rating,
-				numberOfRatings: numberOfRatings
-			});
-			mongoose.disconnect();
-		});
-	});
-});
-
-router.post('/', function (req, res) {
-	mongoose.connect('mongodb://localhost/meal');
-	var db = mongoose.connection;
-
-	db.on('error', console.error.bind(console, 'connection error:'));
-	db.once('open', function () {
-		Meal.findOne({ name: req.body.meal, restaurant: req.body.restaurant}, function (err, meal) {
-			var rating;
-			if(!meal) {
-				var newMeal = new Meal({
-					name: req.body.meal,
-					restaurant: req.body.restaurant,
-					rating: req.body.rating,
-					numberOfRatings: '1'
-				});
-				newMeal.save( function( err, meal) {
-					if(err) return console.error(err);
-					console.dir(meal);
-					mongoose.disconnect();
-				});
-				rating = newMeal.rating;
-			} else {
-				meal.rating = (Number(meal.numberOfRatings) * Number(meal.rating) + Number(req.body.rating)) / (Number(meal.numberOfRatings) + 1);
-				meal.numberOfRatings += 1;
-				meal.save(function (err, meal) {
-					if(err) return console.error(err);
-					mongoose.disconnect();
-				});
-				rating = meal.rating;
-			}
-			res.json( { rating: rating } );
-		});
-	});
-});
-
-module.exports = router;
+module.exports = router
