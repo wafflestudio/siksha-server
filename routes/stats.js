00dfde4a5150877d9dadcdb820c20a4a8da374fe
Kyusang Jo <joks1997@snu.ac.kr>
2017-02-02 23:35:59 +0900

    revision for the comments

--- routes/stats.js
+++ routes/stats.js
@@ -10,52 +10,58 @@ router.get('/all', function (req, res, next) {
     return next(err)
   } else {
     var ranking = req.query.rank
-    Meal.find({}, {_id: 0}, function (error, meal) {
+    Meal.find({}, { _id: 0, __v: 0 }, function (error, meals) {
       if (error) {
         return next(error)
       } else {
-        return res.send(meal)
+        return res.send(meals[0])
       }
-    }).sort({rating: -1, _id: 1}).skip(ranking-1).limit(1)
+    }).sort({ rating: -1, _id: 1 }).skip(ranking-1).limit(1)
   }
 })
 router.get('/:date', function (req, res, next) {
   var menus = []
-  if (!req.query.rank) {
-    var err = new Error('Ranking is missing from query')
-    err.status = 401
-    return next(err)
-  } else {
-    var ranking = req.query.rank
-    menu.crawl(req.params.date, function (resultString) {
-      var result = JSON.parse(resultString)
-      for (var i in result.data) {
-        var restaurant = result.data[i]
-        for (var j in restaurant.foods) {
-          var meal = restaurant.foods[j]
-          menus.push({
-            meal: meal,
-            restaurant: restaurant.restaurant
-          })
+  if (req.params.date === 'today' || req.params.date === 'tomorrow') {
+    if (!req.query.rank) {
+      var err = new Error('Ranking is missing from query')
+      err.status = 401
+      return next(err)
+    } else {
+      var ranking = req.query.rank
+      menu.crawl(req.params.date, function (resultString) {
+        var result = JSON.parse(resultString)
+        for (var i in result.data) {
+          var restaurant = result.data[i]
+          for (var j in restaurant.foods) {
+            var meal = restaurant.foods[j]
+            menus.push({
+              meal: meal,
+              restaurant: restaurant.restaurant
+            })
+          }
         }
-      }
-      Meal.find({}, {_id: 0}, function (error, meals) {
-        if (error) {
-          return next(error)
-        } else {
-          for (var i in meals) {
-            for (var j in menus) {
-              if (meals[i].name === menus[j].meal.name && meals[i].restaurant === menus[j].restaurant) {
-                return res.send(meals)
+        Meal.find({}, { _id: 0, __v: 0 }, function (error, meals) {
+          if (error) {
+            return next(error)
+          } else {
+            for (var i in meals) {
+              for (var j in menus) {
+                if (meals[i].name === menus[j].meal.name && meals[i].restaurant === menus[j].restaurant) {
+                  return res.send(meals[0])
+                }
               }
             }
+            var err = new Error('Rank number out of range')
+            err.status = 400
+            return next(err)
           }
-          var err1 = new Error('Rank number out of range')
-          err1.status = 400
-          return next(err1)
-        }
-      }).sort({rating: -1}).skip(ranking-1).limit(1)
-    })
+        }).sort({ rating: -1 }).skip(ranking-1).limit(1)
+      })
+    }
+  } else {
+    var err = new Error('Date parameter form is wrong')
+    err.status = 400
+    return next(err)
   }
 })
 
