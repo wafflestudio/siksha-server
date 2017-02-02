e9a29c069751593c88c9f248c8478c658e72c15c
Kyusang Jo <joks1997@snu.ac.kr>
2017-02-02 19:21:33 +0900

    changing date form to parameter

--- routes/stats.js
+++ routes/stats.js
@@ -19,7 +19,7 @@ router.get('/all', function (req, res, next) {
     }).sort({rating: -1, _id: 1}).skip(ranking-1).limit(1)
   }
 })
-router.get('/today', function (req, res, next) {
+router.get('/:date', function (req, res, next) {
   var menus = []
   if (!req.query.rank) {
     var err = new Error('Ranking is missing from query')
@@ -27,7 +27,7 @@ router.get('/today', function (req, res, next) {
     return next(err)
   } else {
     var ranking = req.query.rank
-    menu.crawl('today', function (resultString) {
+    menu.crawl(req.params.date, function (resultString) {
       var result = JSON.parse(resultString)
       for (var i in result.data) {
         var restaurant = result.data[i]
@@ -58,43 +58,5 @@ router.get('/today', function (req, res, next) {
     })
   }
 })
-router.get('/tomorrow', function (req, res, next) {
-  var menus = []
-  if (!req.query.rank) {
-    var err = new Error('Ranking is missing from query')
-    err.status = 401
-    return next(err)
-  } else {
-    var ranking = req.query.rank
-    menu.crawl('tomorrow', function (resultString) {
-      var result = JSON.parse(resultString)
-      for (var i in result.data) {
-        var restaurant = result.data[i]
-        for (var j in restaurant.foods) {
-          var meal = restaurant.foods[j]
-          menus.push({
-            meal: meal,
-            restaurant: restaurant.restaurant
-          })
-        }
-      }
-      Meal.find({}, {_id: 0}, function (error, meals) {
-        if (error) {
-          return next(error)
-        } else {
-          for (var i in meals) {
-            for (var j in menus) {
-              if (meals[i].name === menus[j].meal.name && meals[i].restaurant === menus[j].restaurant) {
-                return res.send(meals)
-              }
-            }
-          }
-          var err1 = new Error('Rank number out of range')
-          err1.status = 400
-          return next(err1)
-        }
-      }).sort({rating: -1}).skip(ranking-1).limit(1)
-    })
-  }
-})
+
 module.exports = router
